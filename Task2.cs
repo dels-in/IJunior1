14c62660d3c215f6b061cb3ce4a30abe2a5a1019
dels-in <107535867+dels-in@users.noreply.github.com>
2022-06-15 10:23:25 +0300

    Add files via upload

---
+++ Task2.cs
@@ -0,0 +1,18 @@
+﻿namespace IJunior1;
+
+public class Task2
+{
+    static void Main(string[] args)
+    {
+        int celoe = 5;
+        double racionalnoe = 100.4;
+        float deystvitelnoe = 100.4f;
+        string stroka = "ъуъ";
+        char simvol = '1';
+        long dlinnoeCeloe = 30000000000;
+        bool isIStudent = true;
+        var niRybaNiMyaso = 40.5;
+        byte naturalnoe = 254;
+        decimal desyatichnoe = 0.0000000000000000000000000000000045m;
+    }
+}
