1a52e37c24b4d5e6e23610ea109cada3962cf3be
dels-in <107535867+dels-in@users.noreply.github.com>
2022-06-15 17:20:42 +0300

    Update Task8.cs

--- Task8.cs
+++ Task8.cs
@@ -7,8 +7,8 @@ public class Task8
         Console.WriteLine("Сколько человек в очереди?");
         int grannyNumber = Int32.Parse(Console.ReadLine());
         int grannyTime = 10;
-        int queueHour = (grannyNumber * grannyTime)/60;
-        int queueMinute = (int)Math.Round(((double)grannyNumber * (double)grannyTime / 60 - queueHour)*60);
+        int queueHour = (grannyNumber * grannyTime) / 60;
+        int queueMinute = (grannyNumber * grannyTime) % 60;
         Console.WriteLine($"Вам нужно отстоять {queueHour} часа(ов) {queueMinute} минут(ы).");
     }
 }
